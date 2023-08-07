cb0559747f5c76a597f5e2cfc734a48e1e23d0a1
Ayush Singhal <ayushsinghals02@gmail.com>
2023-08-07 15:52:49 +0530

    Initial Commit

---
+++ Bhagavad Gita/Bhagavad_GitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bhagavad_GitaApp.swift
+//  Bhagavad Gita
+//
+//  Created by Ayush Singhal on 07/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Bhagavad_GitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bhagavad Gita/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bhagavad Gita
+//
+//  Created by Ayush Singhal on 07/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
